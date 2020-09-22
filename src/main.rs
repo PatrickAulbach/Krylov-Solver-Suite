967c7d04e25cfa6f5b4d3cd1a103ed83113ae3d3
PatrickAulbach <Patrick.Aulbach@gmx.net>
2020-09-22 23:15:28 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+#[path = "vector_operations/vector_operations.rs"] mod vector_operations;
+#[path = "matrix_vector_operations/matrix_vector_operations.rs"] mod matrix_vector_operations;
+
+fn main() {
+
+}
