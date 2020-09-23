040a833ce44ba612ab5d25beab2878da7e987af6
PatrickAulbach <Patrick.Aulbach@gmx.net>
2020-09-23 23:32:35 +0200

    rearranged and added new tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::env;
+
 #[path = "vector_operations/vector_operations.rs"] mod vector_operations;
 #[path = "matrix_vector_operations/matrix_vector_operations.rs"] mod matrix_vector_operations;
-#[path = "unit_tests/matrix_vector_operations_unit_tests.rs"] mod tests;
 
 fn main() {
 
--- src/matrix_vector_operations/matrix_vector_operations.rs
+++ src/matrix_vector_operations/matrix_vector_operations.rs
@@ -1,8 +1,14 @@
-#[path = "../vector_operations/vector_operations.rs"] mod vector_operations;
+use std::io::{BufReader, BufRead};
+use std::fs::File;
+use std::env;
+use crate::vector_operations;
 
-pub fn matrix_vector_multiplication(matrix: &Vec<Vec<f64>>, vector: &Vec<f64>, solution: &mut Vec<f64>) {
-
+fn matrix_vector_multiplication(matrix: &Vec<Vec<f64>>, vector: &Vec<f64>, solution: &mut Vec<f64>) {
     let mut buffer: f64 = 0.0;
+
+    if matrix[0].len() != vector.len() {
+        panic!("Dimensions doesn't match. Dimensions are: [{}][{}], [{}]", matrix.len(), matrix[0].len(), vector.len());
+    }
 
     for i in 0..vector.len() {
         for j in 0..matrix[0].len() {
@@ -13,7 +19,7 @@ pub fn matrix_vector_multiplication(matrix: &Vec<Vec<f64>>, vector: &Vec<f64>, s
     }
 }
 
-pub fn compute_residuum(matrix: &Vec<Vec<f64>>, vector: &Vec<f64>, right_hand_side: &Vec<f64>) -> f64 {
+fn compute_residuum(matrix: &Vec<Vec<f64>>, vector: &Vec<f64>, right_hand_side: &Vec<f64>) -> f64 {
     let mut solution: Vec<f64> = Vec::new();
     let mut negative_right_hand_side: Vec<f64> = Vec::new();
 
@@ -26,4 +32,43 @@ pub fn compute_residuum(matrix: &Vec<Vec<f64>>, vector: &Vec<f64>, right_hand_si
     vector_operations::vector_addition(&mut solution, &negative_right_hand_side);
 
     return vector_operations::euclidean_norm(&solution);
+}
+
+fn read_matrix_from_file(path: &str) -> Vec<Vec<f64>> {
+
+    let mut file = BufReader::new(File::open("matrix.txt").unwrap());
+
+    let matrix: Vec<Vec<f64>> = file.lines()
+        .map(|f| f.unwrap().split(char::is_whitespace)
+            .map(|number| number.parse().unwrap())
+            .collect())
+        .collect();
+
+    return matrix;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_matrix_vector_multiplication_with_3_times_3_matrix() {
+        let x = vec![vec![1.0, 0.0, 0.0], vec![0.0, 1.0, 0.0], vec![0.0, 0.0, 1.0]];
+        let y = vec![1.0, 1.0, 1.0];
+        let mut solution: Vec<f64> = Vec::new();
+
+        matrix_vector_multiplication(&x, &y, &mut solution);
+
+        assert_eq!(solution, vec![1.0, 1.0, 1.0]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_matrix_vector_multiplication_mismatching_vector_and_matrix_should_panic() {
+        let x = vec![vec![1.0, 0.0, 0.0, 0.0], vec![0.0, 1.0, 0.0, 0.0], vec![0.0, 0.0, 1.0, 0.0]];
+        let y = vec![1.0, 1.0, 1.0];
+        let mut solution: Vec<f64> = Vec::new();
+
+        matrix_vector_multiplication(&x, &y, &mut solution);
+    }
 }
