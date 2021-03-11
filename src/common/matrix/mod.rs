f3a45c3345375d25e2cb49021b196932485fc053
PatrickAulbach <Patrick.Aulbach@gmx.net>
2021-03-11 22:34:59 +0100

    Added Trait for square root

--- src/common/matrix/mod.rs
+++ src/common/matrix/mod.rs
@@ -1,2 +1,3 @@
 pub mod matrix;
 pub mod dimensions;
+pub mod square_root;
--- src/operations/matrix_operations.rs
+++ src/operations/matrix_operations.rs
@@ -1,14 +1,15 @@
 use crate::common::matrix::matrix::{Vector, Matrix};
 use num::Num;
-use std::ops::{Mul, Add};
+use std::ops::{Mul, Add, AddAssign};
 use std::marker::PhantomData;
 use std::str::FromStr;
+use crate::common::matrix::square_root::Sqrt;
 
 pub struct MatrixOperations<T> {
     phantom_data: PhantomData<T>
 }
 
-impl<T> MatrixOperations<T> where T: Mul<Output = T> + Add<Output = T>, T: FromStr + Copy + Num {
+impl<T> MatrixOperations<T> where T: Mul<Output = T> + Add<Output = T>, T: FromStr + Copy + Num + AddAssign + Sqrt {
     //compute alpha * A + beta * B
     pub fn add(a: Matrix<T>, b: Matrix<T>, alpha: T, beta: T) -> Matrix<T> {
         if alpha == T::zero() && beta == T::one() {
@@ -27,19 +28,85 @@ impl<T> MatrixOperations<T> where T: Mul<Output = T> + Add<Output = T>, T: FromS
             Matrix::new(data)
         }
     }
-}
 
-impl<'a, T: 'a> MatrixOperations<T> where &'a T: Mul<Output = &'a T> + Add<Output = &'a T> + FromStr + Copy + Num, T: std::ops::AddAssign + Num + Copy {
-
-    pub fn euclidean_norm(vector: &Vector<T>) -> T {
+    pub fn euclidean_norm(vector: Vector<T>) -> T {
         let mut norm = T::zero();
 
-        for i in 0..vector.ncols() {
-            norm += vector.data()[i][0] * vector.data()[i][0];
+        for i in 0..vector.nrows() {
+            norm += vector.data()[0][i] * vector.data()[0][i];
         }
 
-        norm
+        norm.sqrt()
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_approx_eq::assert_approx_eq;
+
+
+    #[test]
+    fn add_with_alpha_zero_should_return_b() {
+        let first_matrix: Matrix<f64> = Matrix::new(
+            vec![
+                vec![1.0, 1.0, 1.0],
+                vec![1.0, 1.0, 1.0],
+                vec![1.0, 1.0, 1.0]
+            ]
+        );
+        let second_matrix: Matrix<f64> = Matrix::new(
+            vec![
+                vec![1.0, 1.0, 1.0],
+                vec![1.0, 1.0, 1.0],
+                vec![1.0, 1.0, 1.0]
+            ]
+        );
+
+        let added_matrix: Matrix<f64> = MatrixOperations::add(first_matrix, second_matrix, 0f64, 1f64);
+
+        assert_eq!(added_matrix.data()[0], vec![1.0, 1.0, 1.0]);
+        assert_eq!(added_matrix.data()[1], vec![1.0, 1.0, 1.0]);
+        assert_eq!(added_matrix.data()[2], vec![1.0, 1.0, 1.0]);
+    }
+
+    #[test]
+    fn add_with_alpha_zero_and_beta_greater_one_should_return_beta_times_b() {
+        let first_matrix: Matrix<f64> = Matrix::new(
+            vec![
+                vec![1.0, 1.0, 1.0],
+                vec![1.0, 1.0, 1.0],
+                vec![1.0, 1.0, 1.0]
+            ]
+        );
+        let second_matrix: Matrix<f64> = Matrix::new(
+            vec![
+                vec![1.0, 2.0, 3.0],
+                vec![4.0, 5.0, 6.0],
+                vec![7.0, 8.0, 9.0]
+            ]
+        );
+
+        let added_matrix: Matrix<f64> = MatrixOperations::add(first_matrix, second_matrix, 0f64, 2f64);
+
+        assert_eq!(added_matrix.data()[0], vec![2.0, 4.0, 6.0]);
+        assert_eq!(added_matrix.data()[1], vec![8.0, 10.0, 12.0]);
+        assert_eq!(added_matrix.data()[2], vec![14.0, 16.0, 18.0]);
+    }
+
+    #[test]
+    fn euclidian_norm_should_be_correct() {
+        let vector: Vector<f64> = Vector::new(
+            vec![
+                vec![1.0, 2.0, 3.0]
+            ]
+        );
+
+        let norm = MatrixOperations::euclidean_norm(vector);
+
+        assert_approx_eq!(3.74165738677, norm, 1e-3f64);
+    }
+
 }
 
 /*
