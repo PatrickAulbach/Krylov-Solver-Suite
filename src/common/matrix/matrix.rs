078a0bd8cf17f3250a657bbe2b8f982af583d4dd
PatrickAulbach <Patrick.Aulbach@gmx.net>
2021-03-20 18:02:53 +0100

    Refactored matrix struct

--- src/common/matrix/matrix.rs
+++ src/common/matrix/matrix.rs
@@ -2,18 +2,22 @@ use num::Num;
 use std::ops::{Mul, Add};
 use crate::common::matrix::dimensions::DimensionError;
 
-pub type Vector<T> = Matrix<T>;
+pub type Vector<'a, T> = Matrix<'a, T>;
 
 #[derive(Clone)]
-pub struct Matrix<T> {
-    data: Vec<Vec<T>>,
-    rhs: Option<Vec<T>>,
+pub struct Matrix<'a, T> {
+    data: &'a [T],
+    ncols: usize,
+    nrows: usize,
+    rhs: Option<&'a [T]>,
 }
 
 impl<T: Num> Matrix<T> where T: Mul<Output = T> + Add<Output = T> {
-    pub fn new(data: Vec<Vec<T>>) -> Self {
+    pub fn new(data: &[T], ncols: usize, nrows: usize) -> Self {
         Matrix {
-             data,
+            data,
+            ncols,
+            nrows,
             rhs: None
         }
     }
@@ -21,23 +25,22 @@ impl<T: Num> Matrix<T> where T: Mul<Output = T> + Add<Output = T> {
     pub fn check_consistency(&self) -> Result<(), DimensionError> {
 
         type Error = DimensionError;
-        for column in self.data.iter() {
-            if column.len() != self.nrows() {
-                return Err(DimensionError::InvalidDimension);
-            }
+        if self.ncols * self.nrows {
+            return Err(DimensionError::InvalidDimension);
         }
+
         Ok(())
     }
 
     pub fn ncols(&self) -> usize {
-        self.data.len()
+        self.ncols()
     }
 
     pub fn nrows(&self) -> usize {
-        self.data[0].len()
+        self.nrows()
     }
 
-    pub fn data(&self) -> &Vec<Vec<T>> {
+    pub fn data(&self) -> &[T] {
         &self.data
     }
 
